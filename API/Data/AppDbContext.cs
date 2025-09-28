33ef080e26ea145dda53e9c077b7a1ff5e4bd53b
FongFox <phong.tgn.coder@gmail.com>
2025-09-28 15:57:11 +0700

    2. Config migration for generate database
    Add new dependencies, config migration, generate database.

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
+{
+    public DbSet<AppUser> Users { get; set; }
+}
