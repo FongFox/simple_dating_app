d08e4b4f42f01b36f79ab5837e721dd3518bc1be
FongFox <phong.tgn.coder@gmail.com>
2025-09-29 18:53:48 +0700

    11. Update user entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,8 +3,8 @@ namespace API.Entities;
 public class AppUser
 {
     public String Id { get; set; } = Guid.NewGuid().ToString();
-
     public required String DisplayName { get; set; }
-
     public required String Email { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }
